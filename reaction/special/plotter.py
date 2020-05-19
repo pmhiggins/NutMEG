13c12b917e1a1f08c862df4ac2c84813ef11e154
pmhiggins <pete.matt.higgins@gmail.com>
2020-05-19 12:56:28 +0100

    contained a global plot. Is this file even necessary?

--- reaction/special/plotter.py
+++ reaction/special/plotter.py
@@ -4,11 +4,3 @@ import numpy as np
 
 def epsilon(T):
 	return((-0.8292e-6 *(T**3)) + (0.1417e-2 * (T**2)) - (0.9297*T) + (233.76 + 5321/T))
-	
-X = np.linspace(200,400, num=1000)
-Y = []
-for x in X:
-	Y.append(epsilon(x))
-
-plt.plot(X, Y)
-plt.show()
