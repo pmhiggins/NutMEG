dc0ef79bb8c3c0e473c79d8d8b1d4b4768d22d8c
pmhiggins <32197434+pmhiggins@users.noreply.github.com>
2020-07-16 20:10:10 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+   name='NutMEG',
+   version='0.1',
+   install_requires=['numpy', 'pandas', 'reaktoro', 'maptlotlib'], #external packages as dependencies
+)
