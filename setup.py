de400a4d1839f9107d5e8a3a318e17fda48ce82a
pmhiggins <32197434+pmhiggins@users.noreply.github.com>
2020-12-08 18:22:00 +0000

    update requirements

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
    name='NutMEG',
    version='0.1',
-   install_requires=['numpy', 'pandas','matplotlib'], #external packages as dependencies
+   install_requires=['numpy', 'pandas','matplotlib',], #external packages as dependencies
 )
