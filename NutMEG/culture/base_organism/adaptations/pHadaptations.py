634ba86b4fdcb2bd2b3f30aeaafb2cde64832b02
pmhiggins <pete.matt.higgins@gmail.com>
2021-06-04 11:05:02 +0100

    corrected goldman implementation

--- NutMEG/culture/base_organism/adaptations/pHadaptations.py
+++ NutMEG/culture/base_organism/adaptations/pHadaptations.py
@@ -5,17 +5,13 @@ Class for calculating adaptations to adverse pH conditions
 """
 import math
 # helper functions for Flux/permeability calcuation
-def getfluxH(memb_pot, perm, pHint, pHext, area, K=38.7, T=298):
-    print(((10**-pHext)-((10**-pHint)*math.exp(K*memb_pot))))
-    return(perm*(T/298)*((K*memb_pot)/(1-math.exp(K*memb_pot)))*
-      ((10**-pHext)-((10**-pHint)*math.exp(K*memb_pot)))*area*1e3)
+## out - in(exp)
 
-def getfluxOH(memb_pot, perm, pHint, pHext, area, K=38.7, T=298):
-    return(perm*(T/298)*((K*memb_pot)/(1-math.exp(K*memb_pot)))*
-      ((10**(pHext-14))-((10**(pHint-14))*math.exp(K*memb_pot)))*area*1e3)
+## removed 1/T?
 
-def getEnergyPump(pHint, pHext, T=298):
-    return(8.31*T*abs(pHint-pHext))
+def GoldmanEQ(P, K, concs, SA, T, q=1):
+    return P*(q)*(T/298)*(K/(1-math.exp(-q*K)))*(concs['in']-(concs['out']*math.exp(-q*K)))*SA*1e3
+    # return P*(q**2)*(T/298)*(K/(1-math.exp(q*K)))*(concs['out']-(concs['in']*math.exp(q*K)))*SA*1e3
 
 class pHadaptations:
 
@@ -23,16 +19,27 @@ class pHadaptations:
     def __init__(self, host):
         self.host = host # the host oragnism and its assosiated environment.
 
+
+    def _getfluxH(self):
+        K = self.host.memb_pot*96485/(8.31*self.host.locale.env.T)
+        concs = {'in':10**-self.host.pH_interior, 'out':10**-self.host.locale.pH}
+        return GoldmanEQ(self.host.PermH, K, concs, self.host.surfacearea, self.host.locale.env.T, q=1)
+
+    def _getfluxOH(self):
+        K = self.host.memb_pot*96485/(8.31*self.host.locale.env.T)
+        concs = {'in':10**(self.host.pH_interior-14),
+          'out':10**(self.host.locale.pH-14)}
+        return GoldmanEQ(self.host.PermOH, K, concs, self.host.surfacearea, self.host.locale.env.T, q=-1)
+
+    def _getEnergyPump(self):
+        return(8.31*self.host.locale.env.T*abs(self.host.pH_interior-self.host.locale.pH))
+
+
     """ Flux and Permeability calculation [QE Document] """
 
-
     def getFluxPerm_MP(self):
-        # print(self.host.locale.composition['H+'].conc)
-        pHext = -math.log10(self.host.locale.composition['H+'].conc)
 
-        K= 96485/(8.31*self.host.locale.env.T)
-        fluxH = getfluxH(self.host.memb_pot, self.host.PermH, self.host.pH_interior, pHext, self.host.surfacearea, K=K, T=self.host.locale.env.T)
-        fluxOH = getfluxOH(self.host.memb_pot, self.host.PermOH, self.host.pH_interior, pHext, self.host.surfacearea, K=K, T=self.host.locale.env.T)
-        E = getEnergyPump(self.host.pH_interior, pHext, T=self.host.locale.env.T)
-        #abs(fluxOH)
+        fluxH = self._getfluxH()
+        fluxOH = self._getfluxOH()
+        E = self._getEnergyPump()
         return (abs(fluxOH)+abs(fluxH))*E
