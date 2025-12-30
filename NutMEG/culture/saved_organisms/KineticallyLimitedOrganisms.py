cd4bc48b6efc9728a526e27caf9d70f1d53d9d70
pmhiggins <pm.higginsetal@gmail.com>
2025-12-30 16:00:14 -0500

    separate kinetic and growth limiters

--- NutMEG/culture/saved_organisms/KineticallyLimitedOrganisms.py
+++ NutMEG/culture/saved_organisms/KineticallyLimitedOrganisms.py
@@ -17,38 +17,74 @@ class KineticallyLimitedOrganism(NutMEG.horde):
 
     """
 
-    def __init__(self, name, R, rxn,
+    def __init__(self, name, R, rxn, num,
+      kinetic_F_funcs=None,
+      kinetic_F_attrs=None,
+      growth_F_funcs=None,
+      growth_F_attrs=None,
       donor=None, acceptor=None,
-      F_funcs=None,
-      F_attrs=None,
-      rate_func='zeroth order',
-      n_ATP=1.0,
-      num=500, **horde_kwargs):
+      n_ATP=1.,
+      kinetic_rate_func='zeroth order',
+      rate_constant_env=None,
+      max_growth_rate=None,
+      **horde_kwargs):
 
-        NutMEG.horde.__init__(self,
+        print(horde_kwargs)
+
+        respiration_kwargs={
+          'kinetic_forcing_parameters': kinetic_F_funcs,
+          'kinetic_F_attrs': kinetic_F_attrs,
+          'rate_constant_env': rate_constant_env,
+          'rate_func':kinetic_rate_func,
+          'n_ATP':n_ATP}
+
+        horde_kwargs['respiration_kwargs'] = respiration_kwargs
+
+        CHNOPS_kwargs = {
+          'max_growth_rate' : max_growth_rate,
+          'CHNOPS_forcing_parameters': growth_F_funcs,
+          'CHNOPS_F_attrs': growth_F_attrs,
+        }
+
+        horde_kwargs['CHNOPS_kwargs'] = CHNOPS_kwargs
+
+        super().__init__(
           name, R,
           rxn,
           num,
-          n_ATP = n_ATP,
           **horde_kwargs)
 
-        if donor:
-            self.respiration.set_forcing_parameter('F_D', (lambda resp, K_D: resp.host.locale.composition[donor].conc/(resp.host.locale.composition[donor].conc + K_D)), ['K_D'])
-        if acceptor:
-            self.respiration.set_forcing_parameter('F_A', (lambda resp, K_A: resp.host.locale.composition[acceptor].conc/(resp.host.locale.composition[acceptor].conc + K_A)), ['K_A'])
-        if F_funcs:
-            for key, value in F_funcs.items():
-                self.respiration.set_forcing_parameter(key, value[0], value[1])
-        if F_attrs:
-            for key, value in F_attrs.items():
-                self.respiration.F_attrs[key] = value
+        # # if donor:
+        # #     self.respiration.set_forcing_parameter('F_D', (lambda resp, K_D: resp.host.locale.composition[donor].conc/(resp.host.locale.composition[donor].conc + K_D)), ['K_D'])
+        # # if acceptor:
+        # #     self.respiration.set_forcing_parameter('F_A', (lambda resp, K_A: resp.host.locale.composition[acceptor].conc/(resp.host.locale.composition[acceptor].conc + K_A)), ['K_A'])
+        #
+        # # should just pass these to the respirator constructor!
+        # if kinetic_F_funcs:
+        #     for key, value in kinetic_F_funcs.items():
+        #         self.respiration.set_forcing_parameter(key, value[0], value[1])
+        # if kinetic_F_attrs:
+        #     for key, value in kinetic_F_attrs.items():
+        #         self.respiration.F_attrs[key] = value
+        #
+        # if max_growth_rate:
+        #     self.max_growth_rate = max_growth_rate
+        #     # unless explicitly specified otherwise, tell growth calcs to
+        #     # consider nutrient limitation
+        #     self.nutrientlimitation = horde_kwargs.get('nutrientlimitation', True)
+        # if growth_F_funcs:
+        #     for key, value in growth_F_funcs.items():
+        #         self.CHNOPS.set_forcing_parameter(key, value[0], value[1])
+        # if growth_F_attrs:
+        #     for key, value in growth_F_attrs.items():
+        #         self.CHNOPS.F_attrs[key] = value
 
-        self.bm_conc = num*1e-12
+        self.bm_conc = num*self.mass*1000 # g / L
 
-        self.respiration.net_pathway.update_molar_gibbs_from_quotient(
-          updatestdGibbs=False)
-
-        self.update_metabolic_rate()
+        # # not needed if building in the respirator constructor
+        # self.respiration.net_pathway.update_molar_gibbs_from_quotient(
+        #   updatestdGibbs=False)
+        # self.respiration.get_rate()
 
         self.donor_name = donor
         self.acceptor_name = acceptor
@@ -58,8 +94,12 @@ class KineticallyLimitedOrganism(NutMEG.horde):
     def Builtin(cls, ID, R, num=500, db_fn='default', **horde_kwargs):
 
         print(ID)
+        org_date = None
 
-        with open(os.path.dirname(__file__)+'/KLO_db.yaml', 'r') as f:
+        if db_fn == default:
+            db_fn = os.path.dirname(__file__)+'/KLO_db.yaml'
+
+        with open(db_fn, 'r') as f:
             org_data = yaml.safe_load(f)
 
         org_props = None
@@ -74,45 +114,91 @@ class KineticallyLimitedOrganism(NutMEG.horde):
 
         rxn = NutMEG.reaction.reaction(rgts, prods, R.env)
 
-        rxn.rate_constant_env = org_props.get('Bioenergetics')['k_max'] * org_props.get('dry_mass', 1e-12)
+        rxn.rate_constant_env = org_props.get('Bioenergetics')['k_max'] * org_props.get('dry_mass', 3e-13)
 
         # forcing function attributes
-        F_attrs = {
-          'K_A' : org_props.get('F_A', {}).get('K_A', 0.),
-          'K_D' : org_props.get('F_D', {}).get('K_D', 0.),
+        kinetic_F_attrs = {
           'xi' : org_props.get('Bioenergetics', {}).get('xi')
         }
 
 
-        # non-default forcing functions
-        F_funcs = {}
-        for F in ('F_A', 'F_D', 'F_Temp', 'F_pH'):
+        # look for any additional kinetic forcing functions
+        kinetic_F_funcs = {}
+        # org_kinetic_forcing = org_props['KineticForcing'] # a dict containing kinetic forcing data
 
-            F_ID = org_props.get(F, {'ID':'default'}).get('ID')
-            # {'ID':'default'} is used as the catch to make sure changes
-            # aren't made where the forcing function is not applicable.
+        # potential error source below. If doesn't work go back to commented out above.
+        # Trying to make this work when there is no KineticForcing of GrowthForcing
+        # so the KLO can be more flexible
+        org_kinetic_forcing = org_props.get('KineticForcing', {})
 
-            if F_ID != 'default':
-                FFA, FFB = cls.builtin_forcing_funcs(F_ID, R)
-                F_funcs[F] = (FFA, FFB)
+        for F_k, F_vs in org_kinetic_forcing.items():
+
+            this_func_attrs = {}
+            for k, v in F_vs.items():
+                # this is a dict of information relevant to this forcing parameter,
+                # identifying it, citing a source, and providing the
+                # relevant attributes (e.g., for a Monod model: S and K)
+                if k not in ['F_ID', 'func', 'ref', 'note', 'notes']:
+                    # define a unique identifier for this attribute
+                    _ID = k+'_'+F_vs['F_ID']+F_vs['func']
+
+                    # assign its value to F_attrs, which will be stored in the organism
+                    kinetic_F_attrs[_ID] = v
+
+                    # connect the generic attribute names to the unique ones
+                    # e.g., for monod: 'S':'S_DonorMonod' etc
+                    this_func_attrs[k] = _ID
+
+            # setup the forcing function for this mechanism
+            FFA, FFB = cls.builtin_forcing_funcs(F_vs['func'], this_func_attrs)
+            kinetic_F_funcs[F_k] = (FFA, FFB)
+
+            # F_ID = org_props.get(F, {'ID':'default'}).get('ID')
+            # # {'ID':'default'} is used as the catch to make sure changes
+            # # aren't made where the forcing function is not applicable.
+            #
+            # if F_ID != 'default':
+            #     FFA, FFB = cls.builtin_forcing_funcs(F_ID, R)
+            #     F_funcs[F] = (FFA, FFB)
+
+            # repeat the process above for growth forcing functions
+            growth_F_funcs = {}
+            growth_F_attrs = {}
+            org_growth_forcing = org_props.get('GrowthForcing', {}) # a dict containing growth forcing data
+            for F_k, F_vs in org_growth_forcing.items():
+                this_func_attrs = {}
+                for k, v in F_vs.items():
+                    if k not in ['F_ID', 'func', 'ref', 'note', 'notes']:
+                        _ID = k+'_'+F_vs['F_ID']+F_vs['func']
+                        growth_F_attrs[_ID] = v
+                        this_func_attrs[k] = _ID
+                FFA, FFB = cls.builtin_forcing_funcs(F_vs['func'], this_func_attrs)
+                growth_F_funcs[F_k] = (FFA, FFB)
 
 
-        return cls(ID, R, rxn,
+        return cls(ID, R, rxn, num,
           donor=org_props.get('Donor'),
           acceptor=org_props.get('Acceptor'),
-          F_funcs=F_funcs,
-          F_attrs=F_attrs,
-          rate_func='zeroth order',
-          n_ATP=org_props.get('Bioenergetics').get('n_ATP'),
-          num=num, **horde_kwargs)
+          kinetic_F_funcs=kinetic_F_funcs,
+          kinetic_F_attrs=kinetic_F_attrs,
+          growth_F_funcs=growth_F_funcs,
+          growth_F_attrs=growth_F_attrs,
+          kinetic_rate_func='zeroth order',
+          n_ATP=org_props.get('Bioenergetics').get('n_ATP', 1.0),
+          max_growth_rate=org_props.get('Bioenergetics').get('mu_max', None),
+           **horde_kwargs)
 
 
 
     @staticmethod
-    def builtin_forcing_funcs(ID, R):
+    def builtin_forcing_funcs(funcID, attrs):
 
-        if ID == 'MineralGoethite':
-            return (lambda resp, K_A: (resp.host.bm_conc/resp.host.locale.composition['Goethite'].conc)/((resp.host.bm_conc/resp.host.locale.composition['Goethite'].conc) + K_A)), ['K_A']
+        if funcID == 'Monod':
+            # 2 attrs: substrate ID (e.g., 'H2(aq)'), and Monod half-saturation constant
+            return (lambda resp, S, K: resp.host.locale.composition[S].conc/(resp.host.locale.composition[S].conc + K), [attrs['S'], attrs['K']])
+        if funcID == 'MineralGoethite':
+            return (lambda resp, K: (resp.host.bm_conc/resp.host.locale.composition['Goethite'].conc)/((resp.host.bm_conc/resp.host.locale.composition['Goethite'].conc) + K)), [attrs['K']]
+
         else:
             raise ValueError('Unknown custom forcing function bassed to builtin_forcing_funcs')
 
