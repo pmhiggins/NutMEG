b82ddadc87e484918eeaea862c5b8b1562231d72
pmhiggins <pm.higginsetal@gmail.com>
2026-04-14 20:00:05 -0400

    [untested] models outsourced for metaboliser

--- NutMEG/core/base_organism/metaboliser.py
+++ NutMEG/core/base_organism/metaboliser.py
@@ -3,12 +3,12 @@ import sys, warnings
 sys.path.append("../..")
 import math
 from NutMEG import reaction as rxn
-from NutMEG.environment import environment
-from NutMEG.reactor import reactor
+# from NutMEG.environment import environment
+# from NutMEG.reactor import reactor
 
-import NutMEG.util.NutMEGparams as nmp
-from NutMEG.util.loggersetup import loggersetup as logset
-logger = logset.get_logger(__name__, filelevel=nmp.filelevel, printlevel=nmp.printlevel)
+# import NutMEG.util.NutMEGparams as nmp
+# from NutMEG.util.loggersetup import loggersetup as logset
+# logger = logset.get_logger(__name__, filelevel=nmp.filelevel, printlevel=nmp.printlevel)
 
 class respirator:
     """
@@ -76,25 +76,49 @@ class respirator:
     # rate = None # the actual rate, corrected for other limiters in
       # the organism.
 
+
+
     def __init__(self, host, net_pathway,
-      n_ATP=1., max_metabolic_rate=None,
-      rate_func='first order', rate_func_args={},
-      kinetic_forcing_parameters=None, kinetic_F_attrs=None,
-      rate_constant_env=None, rate_constant_RTP=None,
-      celldata=[0.0001, 0.004, 0.005, 7.], G_ATP=None,
-      overwrite_net_pathway=False, G_net_pathway=None, pathwaytype=None,
-      n_T=None, n_HP=None, n_HR=None, G_C=None):
+      base_rate,
+      forcing_factors,
+      aggregator,
+      overwrite_net_pathway=False):
+
+      # n_ATP=1., max_metabolic_rate=None,
+      # rate_func='first order', rate_func_args={},
+      # kinetic_forcing_parameters=None, kinetic_F_attrs=None,
+      # rate_constant_env=None, rate_constant_RTP=None,
+      # celldata=[0.0001, 0.004, 0.005, 7.], G_ATP=None,
+      # overwrite_net_pathway=False, G_net_pathway=None, pathwaytype=None,
+      # n_T=None, n_HP=None, n_HR=None, G_C=None):
         """
         Parameters
         ----------
-        celldata : list
-            Concentration in the form [activity ADP, activity P, activity ATP, pH]
+        host : ``base_organism`` like
+            host organism. Ensure that the host organism's locale object is the
+            reactor you want.
+        net_pathway : ``reaction`` like or str
+            The overall metabolism to use. In any case it is best to pass a
+            reaction like object (reaction or redox) which will then be unified
+            with ``host.locale``. If a string is passed, look in ``host.locale``
+            for the reaction and set that as the pathway.
+        base_rate : ``BaseRateModel''
+            Model to be used for calculating the maximum metabolic rate
+            in environmental conditions (i.e., before forcing, if present)
+        forcing_factors : list[ForcingFactor]
+            List of ForcingFactor models to use to estimate the actual metabolic
+            rate.
+        aggregator : ``RateAggregator''
+            method to use to select the actual rate from the forcing factors
+            (e.g., Multiplicative, LeibigMinimum, etc.)
+        overwrite_net_pathway : bool, optional
+            Pass if net_pathway has been created but not yet unified with
+            the host's locale.
         G_net_pathway : NoneType or float
             If you want to pass the gibbs gree energy of the metabolic reaction
             use this, if not leave as None. Default ``None``.
         """
         self.host = host
-        self.locale = host.locale
 
 
         #### unify the pathway with the local environment
@@ -113,136 +137,142 @@ class respirator:
         else:
             self.net_pathway = self.locale.reactionlist[net_pathway.equation][type(net_pathway)]
 
-        #### set up overall free energy of metabolic reaction
 
+        #### adjust overall free energy of metabolic reaction if requested
         if G_net_pathway:
-            self.G_A = G_net_pathway
             self.net_pathway.molar_gibbs = self.G_A
-        else:
+        # else:
             # get the molar gibbs from the net pathway reaction ourselves
-            self.net_pathway.rto_current_env()
-            self.net_pathway.update_molar_gibbs_from_quotient(
-              updatestdGibbs=False)
-            self.G_A = self.net_pathway.molar_gibbs
+            # self.net_pathway.rto_current_env()
+            # self.net_pathway.update_molar_gibbs_from_quotient(
+              # updatestdGibbs=False)
 
 
 
-        #### set up metabolic rates
+        #### set up kinetics
+        self.base_rate = base_rate
+        self.forcing_factors = forcing_factors
+        self.aggregator = aggregator
 
         # if a rate constant is passed, the net_pathway's rate constant will be
         # overwritten. If not, it will be left untouched.
-        if rate_constant_env:
-            self.net_pathway.rate_constant_env=rate_constant_env
-        if rate_constant_RTP:
-            self.net_pathway.rate_constant_RTP=rate_constant_RTP
+        # if rate_constant_env:
+            # self.net_pathway.rate_constant_env=rate_constant_env
+        # if rate_constant_RTP:
+            # self.net_pathway.rate_constant_RTP=rate_constant_RTP
 
-        if not self.net_pathway.bool_rate_constants():
-            warnings.warn('Respirator initiated without any rate constants')
+        # if not self.net_pathway.bool_rate_constants():
+        #     warnings.warn('Respirator initiated without any rate constants')
              # unknown, we cannnot' + \
             # 'calucate respiration rates without them!')
-        else:
-            if self.net_pathway.rate_constant_RTP and not self.net_pathway.rate_constant_env:
-                # if we don't know the rate constant outside RTP,
-                # it often changes by 2x every increase by 10 K.
-                self.net_pathway.rate_constant_env = ( \
-                  self.net_pathway.rate_constant_RTP * \
-                  (2**((self.locale.env.T-298)/10)))
+        # else:
+        #     if self.net_pathway.rate_constant_RTP and not self.net_pathway.rate_constant_env:
+        #         # if we don't know the rate constant outside RTP,
+        #         # it often changes by 2x every increase by 10 K.
+        #         self.net_pathway.rate_constant_env = ( \
+        #           self.net_pathway.rate_constant_RTP * \
+        #           (2**((self.locale.env.T-298)/10)))
 
         #### set up rate function.
-        self.max_metabolic_rate = max_metabolic_rate
-        if rate_func == 'first order':
-            self.rate_func_ID = 'first order'
-            self.rate_func = self._rf_first_order
-        elif rate_func == 'Arrhenius':
-            self.rate_func_ID = 'Arrhenius'
-            self.rate_func = self.net_pathway.calculate_rate
-        elif rate_func == 'zeroth order':
-            self.rate_func_ID = 'zeroth order'
-            self.rate_func = lambda: self.net_pathway.rate_constant_env
-            self.max_metabolic_rate = self.net_pathway.rate_constant_env
-        else:
-            self.rate_func = rate_func
-        self.rate_func_args = rate_func_args
+        # self.max_metabolic_rate = max_metabolic_rate
+        # if rate_func == 'first order':
+        #     self.rate_func_ID = 'first order'
+        #
+        # elif rate_func == 'Arrhenius':
+        #     self.rate_func_ID = 'Arrhenius'
+        # elif rate_func == 'zeroth order':
+        #     self.rate_func_ID = 'zeroth order'
+        # else:
+        #     self.rate_func = rate_func
+        # self.rate_func_args = rate_func_args
 
 
         #### setup forcing parameters for respiration
         # Load default forcing functions
-        self.forcing_parameters = {}
-        self.F_attrs = {}
-        if not kinetic_F_attrs:
-            self.F_attrs = {'xi':1.0}
-        else:
-            self.F_attrs = kinetic_F_attrs
-            self.F_attrs['xi'] = kinetic_F_attrs.get('xi', 1.0)
-        self._set_default_forcing()
-
-        if kinetic_forcing_parameters:
-            for name, (func, arg_keys) in kinetic_forcing_parameters.items():
-                self.set_forcing_parameter(name, func, arg_keys)
-
-        self.get_rate()
-
-
+        # self.forcing_parameters = {}
+        # self.F_attrs = {}
+        # if not kinetic_F_attrs:
+        #     self.F_attrs = {'xi':1.0}
+        # else:
+        #     self.F_attrs = kinetic_F_attrs
+        #     self.F_attrs['xi'] = kinetic_F_attrs.get('xi', 1.0)
+        # self._set_default_forcing()
+        #
+        # if kinetic_forcing_parameters:
+        #     for name, (func, arg_keys) in kinetic_forcing_parameters.items():
+        #         self.set_forcing_parameter(name, func, arg_keys)
+        #
+        # self.get_rate()
 
 
 
+    def compute_rate(self):
+        # calulate the current maximum rate.
+        # This needs to be computed every time because some max rate methods
+        # depend on the current environmental conditions
+        self.max_rate = self.base_rate.compute(self)
 
-
-
-    def set_forcing_parameter(self, name, func, arg_keys):
-        """
-        Set or override a forcing function and define its required arguments.
-        Note that your function MUST take a respiration object as its first argument.
-        Names of remaining arguments should be listed in arg_keys. Be sure to update
-        respirator.F_attrs with the arguments you wish to use, or you will recieve a cryptic error.
-        """
-        if not callable(func):
-            raise ValueError(f"Forcing function for {name} must be callable.")
-        if not isinstance(arg_keys, list):
-            raise TypeError(f"Argument keys for {name} must be a list of parameter names.")
-
-        self.forcing_parameters[name] = (func, arg_keys)
+        values = [f.compute(self) for f in self.factors]
+        self.rate =  self.aggregator.combine(self.max_rate, values)
+        return self.rate
 
 
 
 
-    def get_forcing_fraction(self, F_ID):
-        """
-        For forcing parameter with identifier F_ID, return the instantaneous
-        rate forcing (usually between 0 and 1).
-        """
-        try:
-            func, arg_keys = self.forcing_parameters[F_ID]
-            args = [self.F_attrs[key] for key in arg_keys if key in self.F_attrs]
-            return func(self, *args)  # Apply the forcing function
-        except:
-            warnings.warn("Forcing parameter: '"+F_ID+" not present for "+self.host.name)
-            return None
+    # def set_forcing_parameter(self, name, func, arg_keys):
+    #     """
+    #     Set or override a forcing function and define its required arguments.
+    #     Note that your function MUST take a respiration object as its first argument.
+    #     Names of remaining arguments should be listed in arg_keys. Be sure to update
+    #     respirator.F_attrs with the arguments you wish to use, or you will recieve a cryptic error.
+    #     """
+    #     if not callable(func):
+    #         raise ValueError(f"Forcing function for {name} must be callable.")
+    #     if not isinstance(arg_keys, list):
+    #         raise TypeError(f"Argument keys for {name} must be a list of parameter names.")
+    #
+    #     self.forcing_parameters[name] = (func, arg_keys)
 
 
-    def get_rate(self):
-        """
-        Update the rate of reaction in the last state this object
-        was left in. Applies kinetic forcing parameters based on environement,
-        if they are included in the respirator'a forcing_parameters attribute.
-        """
-        self.G_A = self.net_pathway.molar_gibbs
 
-        rate_modifier = 1.
-        # loop through all defined forcing functions
-        # this is implementing the multiplicative monod model.
-        for name, (func, arg_keys) in self.forcing_parameters.items():
-            # Extract necessary arguments from the environment
-            args = [self.F_attrs[key] for key in arg_keys if key in self.F_attrs]
-            rate_modifier *= func(self, *args)  # Apply the forcing function
-            # print(arg_keys, func(self, *args), rate_modifier)
 
-        self.rate = rate_modifier * self.rate_func(*self.rate_func_args)
+    # def get_forcing_fraction(self, F_ID):
+    #     """
+    #     For forcing parameter with identifier F_ID, return the instantaneous
+    #     rate forcing (usually between 0 and 1).
+    #     """
+    #     try:
+    #         func, arg_keys = self.forcing_parameters[F_ID]
+    #         args = [self.F_attrs[key] for key in arg_keys if key in self.F_attrs]
+    #         return func(self, *args)  # Apply the forcing function
+    #     except:
+    #         warnings.warn("Forcing parameter: '"+F_ID+" not present for "+self.host.name)
+    #         return None
 
-        # if there is a hard-coded max rate, check that we have not exceeded it.
-        if self.max_metabolic_rate:
-            if self.rate > self.max_metabolic_rate:
-                self.rate = self.max_metabolic_rate
+
+    # def get_rate(self):
+    #     """
+    #     Update the rate of reaction in the last state this object
+    #     was left in. Applies kinetic forcing parameters based on environement,
+    #     if they are included in the respirator'a forcing_parameters attribute.
+    #     """
+    #     self.G_A = self.net_pathway.molar_gibbs
+    #
+    #     rate_modifier = 1.
+    #     # loop through all defined forcing functions
+    #     # this is implementing the multiplicative monod model.
+    #     for name, (func, arg_keys) in self.forcing_parameters.items():
+    #         # Extract necessary arguments from the environment
+    #         args = [self.F_attrs[key] for key in arg_keys if key in self.F_attrs]
+    #         rate_modifier *= func(self, *args)  # Apply the forcing function
+    #         # print(arg_keys, func(self, *args), rate_modifier)
+    #
+    #     self.rate = rate_modifier * self.rate_func(*self.rate_func_args)
+    #
+    #     # if there is a hard-coded max rate, check that we have not exceeded it.
+    #     if self.max_metabolic_rate:
+    #         if self.rate > self.max_metabolic_rate:
+    #             self.rate = self.max_metabolic_rate
 
 
 
@@ -261,14 +291,3 @@ class respirator:
             if r.name != 'H2O(aq)' and r.name != 'H+' and r.name != 'OH-':
                 ED.append(r.conc*-self.G_A/mr)
         return min(ED)
-
-
-    def _rf_first_order(self):
-        """
-        Generic first order rate law to be added to rate_laws.
-        """
-        conc_multiplier = 1.0
-        for r, mr in self.net_pathway.reactants.items():
-            conc_multiplier = conc_multiplier*(r.activity**mr)
-
-        return self.net_pathway.rate_constant_env * conc_multiplier
