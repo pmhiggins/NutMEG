09046486d147c61fd6dabb5684259e67f5cf65bd
pmhiggins <pm.higginsetal@gmail.com>
2026-05-26 17:20:41 -0400

    additional speciation presets

--- NutMEG/presets/enceladus.py
+++ NutMEG/presets/enceladus.py
@@ -40,14 +40,20 @@ class Enceladus:
             return Enceladus.get_Enceladus_Higgins2024(enc_kwargs, **rtr_kwargs)
 
 
-    def get_Enceladus_Higgins2024(pH_bo=8., Cl=0.1,
+    def get_Enceladus_Higgins2024(pH_bo=8., Cl=0.1, P=1,
       spec_model='pitzerPHREEQC',
       mixingratios='default',
       chemical_species = 'default',
+      from_fn = None,
       **rtr_kwargs):
         """
         Return a Reactor containing an Enceladus-like composition based on
         those calculated in Higgins et al., (2024) JGR:Planets.
+
+        Only a subset of the chemical speciations in that work are available.
+        To read in additional speciation results, they can be downloaded from
+        the Higgins et al., (2024) dataset and the filename passed to the kwarg
+        `from_fn`.
 
         Parameters
         ----------
@@ -55,7 +61,12 @@ class Enceladus:
             pH of the ocean at 273.15 K. Default 8.
         Cl : float, optional
             Molality of Cl, which defines the chemical speciation output to
-            retrieve. Values should be between 0.05 and 0.4. Default 0.1
+            retrieve. Values can be either 0.05, 0.1, 0.2, 0.4. For more fidelity
+            in Cl molality, manually read in chemical speciation data from the
+            zenodo archive referenced in Higgins et al., (2024).
+        P : float, optional
+            Pressure in bar. Value can be 1 or 100 to match with the Higgins
+            et al (2024) speciation endmembers.
         mixingratios : dict, optional
             Dict of gas mixing ratios to incorporate into the ocean. Default
             is to use the nominal values of the mixing ratios reported by
@@ -63,9 +74,13 @@ class Enceladus:
         chemical_species : list, optional
             List of chemical species to extract from the Higgins et al 2024
             chemical speciation and add to the returned ``Reactor``.
+        from_fn : str, optional
+            Path to filename of a speciation output from Higgins et al. (2024).
+            If this is not None, that file will be used regardless of other Cl
+            or P values passed. pH_bo (and T if not 273K) is still required.
         **rtr_kwargs : dict
             Additional kwargs for the ``Reactor`` being built. Most relevant
-            to this preset are 'T' and 'P'.
+            to this preset is the temperature 'T'.
         """
 
         _mixingratios = None
@@ -82,7 +97,7 @@ class Enceladus:
         EncDefaults = {
           'thermodb' : 'supcrt07-organics', # for max replicability with H+24. SUPCRT was used as no CH4 in phreeqc
           'T' : 273.15,
-          'P' : 1e5,
+          'P' : P*1e5,
           'kgH2O' : 1.}
         EncDefaults.update(rtr_kwargs)
 
@@ -90,16 +105,26 @@ class Enceladus:
 
 
         ### retrieve the chemical speciation
-        specdir = os.path.dirname(__file__)+'/../data/Enceladus/H24speciation/Clconc_'+str(Cl)
-        df = pd.read_csv(specdir+'/spec_1bar_'+spec_model+'.csv')
+        spec_fn = None
+        if from_fn:
+            # use user-defined spec file
+            spec_fn = from_fn
+        else:
+            # use builtin spec file
+            specdir = os.path.dirname(__file__)+'/../data/Enceladus/H24speciation/Clconc_'+str(Cl)
+            spec_fn = specdir+'/spec_'+str(int(P))+'bar_'+spec_model+'.csv'
+        df = pd.read_csv(spec_fn)
         _df = df[df['T'] == _this.T]
         __df = _df[_df['pH_bo'] == pH_bo] #isolates for the line of the df we need.
 
 
         for v in chemical_species:
+            _v = v
+            if v == 'H2O(aq)':
+                _v = 'H2O'
             # the species is not present in the reactor yet
-            g = float(__df['g'+v].iloc[0])
-            m = float(__df['m'+v].iloc[0])
+            g = float(__df['g'+_v].iloc[0])
+            m = float(__df['m'+_v].iloc[0])
             rct = rgt(v, _this, phase='aq',
               amount=(m,'molal'), activity=m*g, gamma_molal=g)
 
