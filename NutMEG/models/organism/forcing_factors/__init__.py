9882b88b180cdb80de616964eecd8cae7e890795
pmhiggins <pm.higginsetal@gmail.com>
2026-07-07 18:08:01 -0400

    add upper and lower limit forcing functions

--- NutMEG/models/organism/forcing_factors/__init__.py
+++ NutMEG/models/organism/forcing_factors/__init__.py
@@ -1,9 +1,12 @@
 from .bioenergetic import Bioenergetic
 from .forcing_factor import ForcingFactor
 from .monod import Monod
+from .upper_limit import UpperLimit
+from .lower_limit import LowerLimit
 from .combo_add import ComboAdd
 
-__all__ = ['ForcingFactor', 'Bioenergetic', 'Monod']
+
+__all__ = ['ForcingFactor', 'Bioenergetic', 'Monod', 'UpperLimit', 'LowerLimit', 'ComboAdd']
 
 def find_ff(key):
     if key == 'ForcingFactor':
@@ -12,5 +15,9 @@ def find_ff(key):
         return Bioenergetic
     if key == 'Monod':
         return Monod
+    if key == 'UpperLimit':
+        return UpperLimit
+    if key == 'LowerLimit':
+        return LowerLimit
     if key == 'ComboAdd':
         return ComboAdd
---
+++ NutMEG/models/organism/forcing_factors/lower_limit.py
@@ -0,0 +1,64 @@
+from .forcing_factor import ForcingFactor
+from collections.abc import Iterable
+
+
+class LowerLimit(ForcingFactor):
+    """
+    Step function like forcing factor for microbial kinetic models. Above some
+    limit, the factor value is 0., below it is 1.
+
+    Attributes
+    ----------
+    species : str
+        Name of chemical species which is rate-limiting.
+    val : float
+        Lower limit concentration of that species
+    conctype : str
+        Concentration identifier of ``reagent`` to use. Can be either 'conc',
+        'molal', 'activity'.
+    """
+
+    def __init__(self, species, val, conctype='molality'):
+        """
+        Extends ForcingFactor.__init__()
+
+        Parameters
+        ----------
+        species : str
+            Name of chemical species which is rate-limiting.
+        val : float
+            Lower limit concentration of that species
+        conctype : str
+            Concentration identifier of ``reagent`` to use. Can be either 'conc',
+            'molal', 'activity'.
+        """
+        self.species = species
+        self.val = val
+        self.conctype=conctype
+
+
+    def compute(self, host, locale):
+        """
+        Calculate and return the LowerLimit forcing factor.
+
+        Parameters
+        ----------
+        host : base_organism
+            Host organism. Can be passed as None for this ForcingFactor.
+        locale : reactor
+            Host chemical reactor. Must have a correct concentration of
+            ``substrate``.
+        """
+        S = None
+        if self.conctype == 'molality':
+            S = locale.composition[self.species].molality
+        elif self.conctype == 'molarity':
+            S = locale.composition[self.species].molarity
+        elif self.conctype == 'activity':
+            S = locale.composition[self.species].activity
+        if isinstance(self.val, Iterable):
+            _ret = deepcopy(self.val)
+            for i in range(len(self.val)):
+                _ret[i] = 0. if S < self.val[i] else 1.
+            return _ret
+        return 0. if S < self.val else 1.
